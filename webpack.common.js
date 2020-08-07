a76d809ea17f13d9239e72c647c78390a0cc300b
Daichi Kudo <sakanactor624@gmail.com>
2020-08-07 10:35:47 +0900

    :wrench: split webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -2,17 +2,10 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-  },
-  devServer: {
-    historyApiFallback: true,
-    port: "3030",
-    hot: true,
-    open: false,
   },
   module: {
     rules: [
